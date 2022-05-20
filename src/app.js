37b5890fb2316fe3cc952d8cea16a3f90077197d
Omams21 <emmanuellaogun@gmail.com>
2022-05-20 16:18:24 +0100

    created my propertyendpoint

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import logger from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import authenticationRouter from './routes/authentication';
 
 const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use('/v1', authenticationRouter);
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   res.status(400).json({ error: err.stack });
 });
 
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,11 +2,22 @@ import express from 'express';
 
 import { agentLogin, Agentsignup, } from '../controllers/agent';
 
-import { validateUserSignup } from '../middleware';
+import {
+  agentProperties, allProperties, createProperty, deleteProperty, editProperty, getPropertyById
+} from '../controllers/propertyEndpoints/propertyendpoint';
+import { checkToken } from '../middleware/auth';
+
+import { validatePropertyInput, validateUserSignup } from '../middleware/middleware';
 
 const authenticationRouter = express.Router();
 
 authenticationRouter.post('/signup', validateUserSignup, Agentsignup);
 authenticationRouter.post('/login', agentLogin);
+authenticationRouter.get('/properties', allProperties);
+authenticationRouter.get('/property/:id', getPropertyById);
+authenticationRouter.post('/agent/properties', checkToken, validatePropertyInput, createProperty);
+authenticationRouter.get('/agent/properties/:id', checkToken, agentProperties);
+authenticationRouter.put('/property/:id', checkToken, editProperty);
+authenticationRouter.delete('/property/:id', checkToken, deleteProperty);
 
 export default authenticationRouter;
