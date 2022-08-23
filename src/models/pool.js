bc662fac6597cab74f98b0a03934bf2267f41f32
Omams21 <emmanuellaogun@gmail.com>
2022-08-23 17:11:19 +0100

    added api documentation

--- src/models/pool.js
+++ src/models/pool.js
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
-import { connectionString } from '../settings';
+import { DATABASE_URL } from '../settings';
 
 dotenv.config();
 
-export const pool = new Pool({ connectionString });
+export const pool = new Pool({ DATABASE_URL });
